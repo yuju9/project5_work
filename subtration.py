bc24bc1eef5a3b5ca2f184de78c8436040ee8e94
Jaepyeong <github ljp0822@naver.com>
2024-09-05 10:52:24 +0900

    subtraction

---
+++ subtration.py
@@ -0,0 +1,10 @@
+selectmode = int(input("모드를 선택하세요, 덧셈:0, 뺄셈:1, 곱셈:2, 나눗셈:3"))
+# 두 숫자를 입력받습니다.
+num1 = float(input("첫 번째 숫자를 입력하세요: "))
+num2 = float(input("두 번째 숫자를 입력하세요: "))
+
+# 뺄셈을 수행합니다.
+result = num1 - num2
+
+# 결과를 출력합니다.
+print("결과: ", result)
