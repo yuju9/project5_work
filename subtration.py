aad05f2e7ca9b8ab255ff52c8900a25d3e82e26b
Jaepyeong <github ljp0822@naver.com>
2024-09-05 11:00:35 +0900

    fix sub

--- subtration.py
+++ subtration.py
@@ -1,9 +1,2 @@
-# 두 숫자를 입력받습니다.
-num1 = float(input("첫 번째 숫자를 입력하세요: "))
-num2 = float(input("두 번째 숫자를 입력하세요: "))
-
-# 뺄셈을 수행합니다.
-result = num1 - num2
-
-# 결과를 출력합니다.
-print("결과: ", result)
+def sub(a,b):
+    return a-b
